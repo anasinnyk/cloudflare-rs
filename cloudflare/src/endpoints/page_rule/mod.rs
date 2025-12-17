38e53558d3fd9e360777408f4d649c047940135e
Artem Miroshnychenko <itsysadmin9@gmail.com>
2025-12-17 23:44:09 +0100

    fix(endpoints): re-export all types

--- cloudflare/src/endpoints/page_rule/mod.rs
+++ cloudflare/src/endpoints/page_rule/mod.rs
@@ -5,4 +5,9 @@ pub mod list_page_rule;
 pub mod types;
 pub mod update_page_rule;
 
+pub use create_page_rule::{CreatePageRule, CreatePageRuleParams};
+pub use delete_page_rule::DeletePageRule;
+pub use get_page_rule::GetPageRule;
+pub use list_page_rule::ListPageRules;
 pub use types::*;
+pub use update_page_rule::{UpdatePageRule, UpdatePageRuleParams};
